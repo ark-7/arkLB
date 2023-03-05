a58a780b56ba74d6c868aaf457e21af6335aa7f9
Aadhitya A <aadhitya864@gmail.com>
2023-03-05 20:53:32 +0530

    Make changes
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python
-# Copyright (c) PLUMgrid, Inc.
-# Licensed under the Apache License, Version 2.0 (the "License")
-
-# run in project examples directory with:
-# sudo ./hello_world.py"
-# see trace_fields.py for a longer example
 
 from bcc import BPF
 
