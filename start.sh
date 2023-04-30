1e13ed853128dc2909376c325c7c1c4074cd9284
Aadhitya A <aadhitya864@gmail.com>
2023-04-30 16:22:51 +0530

    Edit script
    Signed-off-by: Aadhitya A <aadhitya864@gmail.com>

--- start.sh
+++ start.sh
@@ -9,3 +9,6 @@ sleep 0.5
 sudo make
 sleep 0.5
 sudo make install
+sleep 2
+echo "arkLB is now installed. Please run arkLB as root user."
+echo "Run sudo make clean to remove arkLB from your system."
